7458bbaaf84fbb57639943153e4f18d67f69b701
Adetunji Adeyinka <adetunjiadeyinka29@gmail.com>
2026-01-03 22:33:36 +0100

    refactor: reorganize imports for better readability

--- extension/src/pages/popup/index.tsx
+++ extension/src/pages/popup/index.tsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom/client";
 import axios from "axios";
+import type React from "react";
+import { useEffect, useState } from "react";
+import ReactDOM from "react-dom/client";
 import "../../style.css";
 
 const Popup = () => {
